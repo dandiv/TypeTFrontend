8b50815af62d4ac5d36bc0b4635c13cf2130a9d0
dandiv <dandiv32@gmail.com>
2023-08-17 18:00:54 +0300

    fix navigation

--- chart.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const navbarContainer = document.createElement("div");
-  navbarContainer.innerHTML = '<object type="text/html" data="navbar.html" />';
-  document.body.prepend(navbarContainer);
-});
