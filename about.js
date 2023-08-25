1d2b4f9c2e4d9617331c3bcff52cfc3f4853102a
dandiv <dandiv32@gmail.com>
2023-08-25 13:48:17 +0300

    adding map

--- about.js
+++ about.js
@@ -1,5 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const navbarContainer = document.createElement("div");
-  navbarContainer.innerHTML = '<object type="text/html" data="navbar.html" />';
-  document.body.prepend(navbarContainer);
-});
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
