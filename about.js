447decebfd36fcb510081c0478434c010ea13242
dandiv <dandiv32@gmail.com>
2023-08-25 15:20:29 +0300

    adding map

--- about.js
+++ about.js
@@ -1,6 +1,28 @@
-function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
+let map;
+// initMap is now async
+async function initMap() {
+  // Request libraries when needed, not in the script tag.
+  const { Map } = await google.maps.importLibrary("maps");
+  // Short namespaces can be used.
+  map = new Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
 }
+
+// Get mark from DB
+marks = [];
+
+// Mark image
+const image =
+  "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+
+// Update map
+initMap();
+marks.forEach((mark) => {
+  newMark = new google.maps.Marker({
+    position: { lat: mark.lat, lng: mark.lng },
+    map,
+    icon: image,
+  });
+});
